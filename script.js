67536fd33ec17b86a3f2a077d7473bc3dd415fde
Vinh Mao <vinhxmao87@gmail.com>
2020-03-19 17:44:11 -0700

    commit 2: initiated all conditions for password, inlcluding cpaital, lowercase, number and special characters

--- script.js
+++ script.js
@@ -1,12 +1,36 @@
 // Write a function that takes user input and creates a password
 // based on that input and returns it
 function generatePassword() {
-  /****
-   * WRITE YOUR CODE HERE
-   */
-  alert("I am in generate password");
+  var capital = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "1234567890";
+  var specialChar = " !#$%&'()*+,-./:;<=>?@[\]^_`{|}~";
+  specialChar += "\"";
+  var finalPassword;
+  var charSet = "";
+  var numberOfChars = parseInt(prompt("How many characters would you like your password to have? Choose a number between 8 and 128, please!"));
+  if (typeof numberOfChars == "number" && numberOfChars >= 8 && numberOfChars <= 128) {
+    var wantsCapital = confirm("Would you like your password to have capital letters?");
+    if (wantsCapital) {
+      charSet += capital;
+    }
+    var wantsLowercase = confirm("Would you like your password to have lowercase letters?");
+    if (wantsLowercase) {
+      charSet += lowercase;
+    }
+    var wantsNumbers = confirm("Would you like your password to have numbers?");
+    if (wantsNumbers) {
+      charSet += numbers;
+    }
+    var wantsSpecial = confirm("Would you like your password to have special characters?");
+    if (wantsSpecial) {
+      charSet += specialChar;
+    }
+  } else {
+    alert("Please type a valid number!");
+  }
+  console.log(charSet);
   
-
   return "sometext";
 }
 
